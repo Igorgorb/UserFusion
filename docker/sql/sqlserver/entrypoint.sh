93f34734d0d4b77096a33cf674fe338fc616d5e6
Ihor Horbonos <igorgorb@gmail.com>
2024-06-09 02:24:12 +0300

    Saving files before refreshing line endings

--- docker/sql/sqlserver/entrypoint.sh
+++ docker/sql/sqlserver/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Run Microsoft SQl Server and initialization script (at the same time)
+# Run Microsoft SQl Server and initialization script (at the same time) 
 #/opt/mssql/bin/sqlservr
 /usr/src/app/run-initialization.sh & /opt/mssql/bin/sqlservr
